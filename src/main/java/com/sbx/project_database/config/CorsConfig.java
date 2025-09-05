610f56025d22cee9ca12e9296c13f0643193880f
zehra-ozkan <zehra.ozkan138@gmail.com>
2025-09-05 11:39:28 +0300

    cors config update

--- src/main/java/com/sbx/project_database/config/CorsConfig.java
+++ src/main/java/com/sbx/project_database/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.sbx.project_database.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -7,15 +8,26 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig { //without cors configuration it doesnt run on chrome
 
+    @Value("${CORS_ALLOWED_ORIGIN:http://localhost:8080}") // Default to localhost for development
+    private String allowedOrigin;
+
     @Bean
     public CorsFilter corsFilter() {
+
+        // Inject the allowed origin from an environment variable
+
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of(allowedOrigin));
+
         config.addAllowedOrigin("http://localhost:8080"); // For web
         config.addAllowedOrigin("http://127.0.0.1:3000");
         config.addAllowedOrigin("http://192.168.1.10:8080");
